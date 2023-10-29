ff369d59f83d4ef9cb5c1081bbeac12bcfb25159
MonaDhote <ingole.pratiksha166@gmail.com>
2023-10-29 11:24:44 +0530

    Bharti is her own coding

---
+++ src/demo/demo.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int localgit;
+		double gitstaging;
+		int bharti;
+
+	}
+
+}
