1a68f3310065d943228ff090067ffa5eeb665b68
MonaDhote <ingole.pratiksha166@gmail.com>
2023-10-29 15:39:31 +0530

    bharti coding

--- src/demo/demo.java
+++ src/demo/demo.java
@@ -1,12 +1,13 @@
 package demo;
 
 public class demo {
+	int localgit;
+	double gitstaging;
+	int bharti;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int localgit;
-		double gitstaging;
-		int bharti;
+	
 
 	}
 
